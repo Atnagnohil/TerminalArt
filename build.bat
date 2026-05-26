41e59bce8ff6f69479b843255971f3baa6c1d520
andinm <15968258+andinm@user.noreply.gitee.com>
2026-05-26 14:48:22 +0800

    chore: update build.bat with uv, add build artifacts to gitignore

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ echo ========================================
 echo   TerminalArt - PyInstaller
 echo ========================================
 
-pyinstaller ^
+uv run pyinstaller ^
   --onefile ^
   --console ^
   --name=TerminalArt ^
